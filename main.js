4b80f7b72a64906a6241c3373b0ea9ff493058a5
Iain <iain.brookes@outlook.com>
2020-12-01 18:49:13 +0000

    Thanks to jake

--- main.js
+++ main.js
@@ -4,10 +4,13 @@ let container = document.querySelector('.container');
 
 function createDivs(numOfDivs) {
 
-    let numOfSquares = numOfDivs * numOfDivs;
+    const numOfSquares = numOfDivs * numOfDivs;
+    const size = (640 / numOfDivs) - 2;
 
     for (let i = 1; i <= numOfSquares; i++) {
         let div = document.createElement('div');
+        div.style.width = size + 'px';
+        div.style.height = size + 'px';
         div.className = "sq";
         container.appendChild(div);
         div.addEventListener("mouseover", function() {
@@ -17,4 +20,4 @@ function createDivs(numOfDivs) {
 
 }
 
-createDivs(16);
+createDivs(64);
