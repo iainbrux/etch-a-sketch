f27465b782b3a5e69e954b7421b0bb850ac31fd1
Iain <iain.brookes@outlook.com>
2020-12-09 00:18:54 +0000

    Added buttons to select grid size

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-let container = document.querySelector('.container');
+let container = document.getElementById('container');
 
-let userInput = prompt("Please enter a number up to and including 64.", 16);
+let userInput = 32;
 
-if (userInput > 64) {
+if (userInput > 64 || userInput < 0) {
     alert("Number is over specified limit. Please enter a number up to 64.");
-    userInput = 16; // sets the default square size to 16
+    userInput = prompt('Please enter a number up to  and including 64.');
+} else if (userInput == undefined) {
+    userInput = 16;
 }
+
 
 // Function takes the userInput and creates the number of divs respective to the sqaure of that number
 // and then calculates the square size in px to prevent overflow from the container
 
-function createDivs(numOfDivs) {
+function createDivs(userInput) {
 
-    const numOfSquares = numOfDivs * numOfDivs;
-    const size = (640 / numOfDivs) - 2;
+    const numOfSquares = userInput * userInput;
+    const size = (640 / userInput) - 2;
 
     for (let i = 1; i <= numOfSquares; i++) {
         let div = document.createElement('div');
@@ -22,19 +25,43 @@ function createDivs(numOfDivs) {
         div.className = "sq";
         container.appendChild(div);
         div.addEventListener("mouseover", function() {
-            div.className = "sq mouseover";
+            div.className = "sq mouseover active-button";
         });
-    };
-
-};
-
-function resetDivs() {
-
-    let div = document.querySelectorAll('.mouseover');
-    for (let i = 0; i <= div.length - 1; i++) {
-        div[i].classList.remove('mouseover');
     }
 
 }
 
-createDivs(userInput);
+function resetDivs() {
+
+    let div = document.querySelectorAll('.mouseover');
+
+    for (let i = 0; i < div.length; i++) {
+        div[i].classList.remove('mouseover', 'active-button');
+    }
+
+}
+
+function removeDivs() {
+
+    let div = document.querySelectorAll('.sq');
+
+    if (container.children) {
+        for (let i = 0; i < div.length; i++) {
+            let square = div[i];
+            container.removeChild(square);
+        }
+    }
+
+}
+
+function sixteen() {
+    removeDivs();
+    createDivs(16);
+}
+
+function thirtyTwo() {
+    removeDivs();
+    createDivs(32);
+}
+
+createDivs(16);
